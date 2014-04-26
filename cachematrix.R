84c70b9827fbcc93cbf344ba7f3f83d8542b62d3
Matt Feinstein <mfeinste@mf-macbook-pro.home>
2014-04-26 13:05:34 -0400

    Programming Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+
+
+## Creates a special 'matrix' object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <-function(solve) inv <<- solve
+	getinv <- function() inv
+	list(set = set, get = get,
+	     setinv = setinv,
+	     getinv = getinv)
+
+}
+
+
+## computes the inverse of matrix returned by makeCacheMatrix
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+        	message("getting cached data")
+        	return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+}
